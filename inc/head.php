d2306854fd9a309f8759dbcfed72363080979171
dcorpi72 <david.corpi@sticmexico.com>
2024-06-21 12:51:45 -0600

    SISTEMA SUBIDO
    SUSTEMA COMPLETO V1

---
+++ inc/head.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Inventario Sistemas | MAVEPO/MAVESA</title>
+<!--Favicon-->
+<link rel="icon" href="../img/LOGO_MAF20241.png">
+<!--CSS-->
+<link rel="stylesheet" href="./css/bulma.min.css">
+<link rel="stylesheet" href="./css/estilos.css">
